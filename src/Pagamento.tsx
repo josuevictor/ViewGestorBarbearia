31a6c9f9d7698968deab226b9f24ade6c3a5bcca
Josue Victor <victorarroxellas01@gmail.com>
2025-02-08 11:05:55 -0300

    rota de pagamento configurada para requsitar a api de pix, e gerar o qrCode em tela

--- src/Pagamento.tsx
+++ src/Pagamento.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import QRCode from 'react-qr-code';
 
 const PaymentScreen: React.FC = () => {
+  const [qrCode, setQrCode] = useState('');
+
+  useEffect(() => {
+    const requestData = {
+      email: "victorarroxellas224@gmail.com",
+      transaction_amount: 50.00
+    };
+
+    fetch('http://127.0.0.1:8000/api/pagamento/pix', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestData)
+    })
+      .then(response => response.json())
+      .then(data => setQrCode(data.qr_code_base64))
+      .catch(error => console.error('Erro ao buscar o código QR:', error));
+  }, []);
+
   return (
     <div>
       <h1>Pagamento da Mensalidade</h1>
-      <p>Aqui o gestor pode realizar o pagamento da mensalidade do sistema.</p>
-      {/* Adicione o formulário ou informações de pagamento aqui */}
+      <p>Aqui você pode realizar o pagamento da mensalidade do sistema.</p>
+      <p>Caso o pagamento nao seja identificado, o acesso será bloqueado após 3 dias.</p>
+      {/* Exibir o QR code */}
+      {qrCode && <img src={`data:image/png;base64,${qrCode}`} alt="QR Code" style={{ width: '200px', height: '200px' }} />}
     </div>
   );
 };
